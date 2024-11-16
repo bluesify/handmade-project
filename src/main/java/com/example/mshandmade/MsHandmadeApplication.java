b74f192ecadb2a74cf96d223be77a67ed60a69c2
bluesify <fuad.abdullazade879@gmail.com>
2024-11-16 23:30:00 +0400

    HP-01 : initial commit

---
+++ src/main/java/com/example/mshandmade/MsHandmadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mshandmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHandmadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsHandmadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mshandmade/MsHandmadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mshandmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHandmadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
